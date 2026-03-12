8d877573a023827a7d470f57057113a6c385d79e
diegato <gonzalezbusnego@gmail.com>
2026-03-12 15:17:42 -0300

    add a commit with wrong things

---
+++ DTO/WrongNameDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace editorconfigExample.DTO;
+
+  public class WrongNameDTO {
+    public string Name { get; set; }
+      public string description { get; set; }
+    public bool isOk { get; set; } = false;
+    }
+
